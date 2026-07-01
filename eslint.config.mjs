d0426ed098f4a96afcf1db8e6b36b57ba89e3eaf
Alejandro Abadía <ajabadia@gmail.com>
2026-07-01 13:39:11 +0200

    Auto-commit/Release: 2026-07-01 13:24:31

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import { fixupConfigRules } from "@eslint/compat";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...fixupConfigRules(nextVitals),
+  ...fixupConfigRules(nextTs),
   {
     files: ["**/*.test.ts"],
     rules: {
